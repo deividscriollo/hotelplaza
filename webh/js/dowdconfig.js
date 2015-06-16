273affc4b23b3c10d6042ca2823f5ebca0e0e8f8
Deivid <deividscriollo@gmail.com>
2015-06-16 10:18:36 -0500

    web

---
+++ webh/js/dowdconfig.js
@@ -0,0 +1,17 @@
+
+// Por dow Developers, config by dc
+// administracion TI de Hotel Plaza Victoria
+$(function(){
+	// animacion de particles background
+		// inicio
+			$('#particles').particleground({
+			    dotColor: 'rgba(255,255,255,0.3)',
+			    lineColor: 'rgba(255,255,255,0.5)'
+			  });
+		// function
+  
+  // $('.intro').css({
+  //   'margin-top': -($('.intro').height() / 2)
+  // });
+
+})
